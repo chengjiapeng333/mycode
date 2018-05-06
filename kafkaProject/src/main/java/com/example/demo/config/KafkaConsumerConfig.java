4bc8e70c8734acde831035204e03e453e7cad566
chengjiapeng <2695061956@qq.com>
2018-05-06 16:44:07 +0800

    修正导入的包

--- kafkaProject/src/main/java/com/example/demo/config/KafkaConsumerConfig.java
+++ kafkaProject/src/main/java/com/example/demo/config/KafkaConsumerConfig.java
@@ -1,8 +1,8 @@
 package com.example.demo.config;
 
 import com.example.demo.listener.UserInfoListener;
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
