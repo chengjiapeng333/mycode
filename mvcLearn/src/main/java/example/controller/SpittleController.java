5b5913946164378f3e63fdf38791ade1fc8bb9a8
chengjiapeng <2695061956@qq.com>
2018-05-17 01:19:11 +0800

    2018-05-17

--- mvcLearn/src/main/java/example/controller/SpittleController.java
+++ mvcLearn/src/main/java/example/controller/SpittleController.java
@@ -1,11 +1,15 @@
 package example.controller;
 
+import example.pojo.Spitter;
+import example.pojo.Spittle;
 import example.repo.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import javax.jws.WebParam;
+import java.util.Date;
 
 /**
  * Created by chengjiapeng on 2018/5/11.
@@ -26,4 +30,27 @@ public class SpittleController {
         return "spittles";
     }
 
+    @RequestMapping(value = "/{spittleId}", method = RequestMethod.GET)
+    public String spittle(@PathVariable("spittleId") long spittleId, Model model) {
+        model.addAttribute("spittle","123");
+        return "spittles";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerForm() {
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String postRegister(Spitter spitter) {
+        return "redirect:/spittles/spitter/"+spitter.getUsername();
+    }
+
+    @RequestMapping(value = "/spitter/{username}", method = RequestMethod.GET)
+    public String profile(@PathVariable String username, Model model) {
+        System.out.println("444444444444444");
+        Spitter spitter = new Spitter(username, username, username, username);
+        model.addAttribute("spitter", spitter);
+        return "profile";
+    }
 }
---
+++ mvcLearn/web/WEB-INF/views/registerForm.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chengjiapeng
+  Date: 2018/5/17
+  Time: 0:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Register</h1>
+    <form method="post">
+        FirstName:<input type="text" name="firstName"/><br/>
+        LastName:<input type="text" name="lastName"/><br/>
+        UserName:<input type="text" name="username"/><br/>
+        Password:<input type="text" name="password"/><br/>
+        <input type="submit" value="Register" />
+    </form>
+
+</body>
+</html>
