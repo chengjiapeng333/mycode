4c76a4750c6cbbaa9655229f5d8cbd75ab8237db
chengjiapeng <2695061956@qq.com>
2018-05-20 03:12:05 +0800

    实体类添加validate注解

--- mvcLearn/src/main/java/example/pojo/Spitter.java
+++ mvcLearn/src/main/java/example/pojo/Spitter.java
@@ -1,15 +1,28 @@
 package example.pojo;
 
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 /**
  * Created by chengjiapeng on 2018/5/17.
  */
 public class Spitter {
+    @NotNull
+    @Size(min = 5, max = 16)
     private String firstName;
 
+    @NotNull
+    @Size(min = 5, max = 16)
     private String lastName;
 
+    @NotNull
+    @Size(min = 5, max = 16)
     private String username;
 
+    @NotNull
+    @Size(min = 5, max = 16)
     private String password;
 
     public Spitter() {
